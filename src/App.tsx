5ee08d2588903f668d4173410ccf55187d91293d
Manjunatha C <manjunathagee@gmail.com>
2024-03-25 19:52:56 +0530

    Tailwind Integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 import "./App.css";
-import ExpandingCards from "./components/DayOne/ExpandingCards";
 
 function App() {
-
   return (
-    <ExpandingCards />
+    <div className="w-[500px] mx-auto m-8 p-4 rounded-lg bg-slate-100/90 flex flex-col gap-2">
+      Hello world
+    </div>
   );
 }
 
